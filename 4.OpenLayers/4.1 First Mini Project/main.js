586d77e00e9d32c6f33e4a8dea377d68aa6cbd27
okomojacob <jaylabs2030@gmail.com>
2021-11-11 15:03:40 +0300

    :bug:, Only stamen terrain loads from the layerGroups

--- 4.OpenLayers/4.1 First Mini Project/main.js
+++ 4.OpenLayers/4.1 First Mini Project/main.js
@@ -19,15 +19,15 @@ function init(){
     // Adding OSM Standard and Humaniterain basemap Layers
     const openStreetMapStandard = new ol.layer.Tile({
         source: new ol.source.OSM(),
-        visibile: true,
+        visibility: fasle,
         title: 'OSMStandard'
     })
 
     const openStreetMapHumanitairian = new ol.layer.Tile({
         source: new ol.source.OSM({
-            url: 'https://tile.openstreetmap.org/${z}/${x}/${y}.png'
+            url: 'http://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png'
         }),
-        visibile: true,
+        visibility: false,
         title: 'OSMHumanitarian'
     })
 
@@ -36,11 +36,12 @@ function init(){
             url: 'https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
             attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
         }),
-        visibile: false,
+        visibility: true,
         title: 'StamenTerrain'
     })
     // Layer Groups
-    // map.addLayer(stamenTerrain);
+    // map.addLayer(openStreetMapStandard);
+    // Buggy, only stamen loads
     const baseLayerGroup = new ol.layer.Group({
         layers: [
             openStreetMapStandard, openStreetMapHumanitairian, stamenTerrain
